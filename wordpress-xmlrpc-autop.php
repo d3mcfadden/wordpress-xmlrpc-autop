98f26dc89c3e76255addb637e0ea8ffdb06150a7
Dan McFadden <d3mcfadden@gmail.com>
2015-04-17 10:44:04 -0400

    initial checkin

---
+++ wordpress-xmlrpc-autop.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Plugin Name: wordpress-xmlrpc-autop
+ * Description: update the post_content to include paragraph tags
+ * Author: Dan McFadden
+ * License: MIT
+ * */
+
+add_filter( 'xmlrpc_prepare_post', 'cpdm_xmlrpc_methods' );
+
+function cpdm_xmlrpc_methods( $post, $orig_post, $fields )
+{
+	if (isset($post['post_content'])) {
+		$post['post_content'] = wpautop($post['post_content']);
+	}
+	return $post;
+}
